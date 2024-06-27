80f08ebb633e429b711f6f01515d80fffe115ef5
FernandoJRR <fercitotron@gmail.com>
2024-06-27 14:00:51 -0600

    add objection and bodyparser dependencies, add structure for users endpoints

---
+++ api-plataforma/src/models/usuario.ts
@@ -0,0 +1,28 @@
+import { Model, RelationMappings, RelationMappingsThunk } from "objection";
+import { Rol } from "./rol";
+
+export class Usuario extends Model {
+    static tableName = 'usuario';
+
+    username?: string;
+    nombre?: string;
+    password?: string;
+    id_rol?: number;
+  
+    static get idColumn() {
+      return 'username';
+    }
+    
+    static get relationMappings(): RelationMappings | RelationMappingsThunk {
+        return {
+          career: {
+            relation: Model.BelongsToOneRelation,
+            modelClass: Rol,
+            join: {
+              from: 'usuario.id_rol',
+              to: 'rol.id',
+            },
+          },
+        };
+    }
+}
