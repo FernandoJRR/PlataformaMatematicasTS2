6788d7b73eca150e4d48fc97f2ca24daf74c30c3
JosePu99 <joseluis-pusam@cunoc.edu.gt>
2024-07-03 22:25:13 -0600

    inicio de endpoints de partidas y progreso partida

--- api-plataforma/src/models/ejercicio_partida.ts
+++ api-plataforma/src/models/ejercicio_partida.ts
@@ -1,4 +1,6 @@
 import { Model, RelationMappings, RelationMappingsThunk } from "objection";
+import { Ejercicio } from "./ejercicio";
+import { Partida } from "./partida";
 
 export class EjercicioPartida extends Model {
     static tableName = 'ejercicio_partida';
@@ -6,4 +8,24 @@ export class EjercicioPartida extends Model {
     id_ejercicio?: number;
     id_partida?: number;
     resuelto_satisfactoriamente?: boolean;
+    static get relationMappings(): RelationMappings | RelationMappingsThunk {
+        return {
+          ejercicio: {
+            relation: Model.HasManyRelation,
+            modelClass: Ejercicio,
+            join: {
+              from: 'ejercicio_partida.id_ejercicio',
+              to: 'ejercicio.id',
+            },
+          },
+          partida: {
+            relation: Model.HasManyRelation,
+            modelClass: Partida,
+            join: {
+              from: 'ejercicio_partida.id_partida',
+              to: 'partida.id',
+            },
+          },
+        };
+    }
 }
--- api-plataforma/src/models/partida.ts
+++ api-plataforma/src/models/partida.ts
@@ -1,10 +1,40 @@
 import { Model, RelationMappings, RelationMappingsThunk } from "objection";
+import { Tema } from "./tema";
 
 export class Partida extends Model {
     static tableName = 'partida';
 
+    id?: number;
     id_tema?: number;
     puntaje?: number;
     username_jugador?: string;
     id_modo_juego?: number;
+    static get relationMappings(): RelationMappings | RelationMappingsThunk {
+        return {
+          tema: {
+            relation: Model.BelongsToOneRelation,
+            modelClass: Tema,
+            join: {
+              from: 'partida.id_tema',
+              to: 'tema.id',
+            },
+          },
+          usuario: {
+            relation: Model.HasManyRelation,
+            modelClass: Tema,
+            join: {
+              from: 'partida.username_jugador',
+              to: 'usuario.username',
+            },
+          },
+          modo_juego: {
+            relation: Model.HasManyRelation,
+            modelClass: Tema,
+            join: {
+              from: 'partida.id_modo_juego',
+              to: 'modo_juego.id',
+            },
+          },
+        };
+    }
 }
