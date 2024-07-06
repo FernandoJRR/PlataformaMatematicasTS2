61ab35cc80a9d687c0952cc4d7122152c234d227
FernandoJRR <fercitotron@gmail.com>
2024-07-06 15:48:38 -0600

    change modos de juego seeder, add endpoints for patch temario, tema and ejercicio

--- api-plataforma/src/services/temario.service.ts
+++ api-plataforma/src/services/temario.service.ts
@@ -115,8 +115,10 @@ export async function addTema(idTemario: number, inputTema: any) {
     }
     
     await trx.commit();
+    return tema;
   } catch (error) {
     await trx.rollback();
+    throw error;
   }
 }
 
@@ -155,4 +157,73 @@ export async function addEjercicio(idTema: number, inputEjercicio: any) {
     await trx.rollback();
   }
   
+}
+
+export async function updateTemario(idTemario: number, input: any) {
+  const temario = await Temario.query()
+    .findById(idTemario);
+  
+  if (temario === undefined) {
+    throw new Error("El temario no existe");
+  }
+
+  const trx = await Temario.startTransaction();
+  try {
+    await Temario.query()
+    .where('id', idTemario)
+    .update(input);
+    
+    await trx.commit();
+    return await Temario.query()
+      .findById(idTemario);
+  } catch (error) {
+    await trx.rollback();
+    throw error;
+  }
+}
+
+export async function updateTema(idTema: number, input: any) {
+  const tema = await Temario.query()
+    .findById(idTema);
+  
+  if (tema === undefined) {
+    throw new Error("El tema no existe");
+  }
+
+  const trx = await Tema.startTransaction();
+  try {
+    await Tema.query()
+    .where('id', idTema)
+    .update(input);
+    
+    await trx.commit();
+    return await Tema.query()
+      .findById(idTema);
+  } catch (error) {
+    await trx.rollback();
+    throw error;
+  }
+}
+
+export async function updateEjercicio(idEjercicio: number, input: any) {
+  const ejercicio = await Ejercicio.query()
+    .findById(idEjercicio);
+  
+  if (ejercicio === undefined) {
+    throw new Error("El ejercicio no existe");
+  }
+
+  const trx = await Ejercicio.startTransaction();
+  try {
+    await Ejercicio.query()
+    .where('id', idEjercicio)
+    .update(input);
+    
+    await trx.commit();
+    return await Ejercicio.query()
+      .findById(idEjercicio);
+  } catch (error) {
+    await trx.rollback();
+    throw error;
+  }
 }
