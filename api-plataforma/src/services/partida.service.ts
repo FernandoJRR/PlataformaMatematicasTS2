1cd0c74e515532e5e58715b65580672fd6cd6f2c
FernandoJRR <fercitotron@gmail.com>
2024-07-07 23:53:56 -0600

    add logro seeders, add first 3 logros

--- api-plataforma/src/services/partida.service.ts
+++ api-plataforma/src/services/partida.service.ts
@@ -4,6 +4,7 @@ import { EjercicioPartida } from "../models/ejercicio_partida";
 import { ModoJuego } from "../models/modo_juego";
 import { Temario } from "../models/temario";
 import { Ejercicio } from "../models/ejercicio";
+import { LogroUsuario } from "../models/logro_usuario";
 
 export async function getPartidas() {
   const partidas = await Partida.query();
@@ -52,6 +53,8 @@ export async function storePartida(input: any) {
       await EjercicioPartida.query()
       .insert(ejercicio)
     }
+
+    //Aqui se verifica si se le daran logros al jugador
     
     await trx.commit();
     return partida;
@@ -61,3 +64,49 @@ export async function storePartida(input: any) {
   }
 }
 
+async function verificarLogros(partida: Partida) {
+  const partidasJugador = await Partida.query()
+  .where('username_jugador', partida.username_jugador!);
+
+  //Se verifica si es la primer partida jugada del jugador
+  if (partidasJugador.length === 1) {
+    //Si lo es se le da el logro Primer Partida
+    await LogroUsuario.query()
+    .insert({id_logro: 1, username_usuario: partida.username_jugador!, fecha_conseguido: new Date()});
+  }
+
+  //Se verifica si es la primer victoria del jugador (60% o mas por primera vez)
+  if (partida.puntaje! >= 0.6) {
+    //Se verifica que el jugador no tenga el logro
+    if (!verificarPosesionLogro(2, partida.username_jugador!)) {
+      //Si no lo tiene se le da
+      await LogroUsuario.query()
+      .insert({id_logro: 2, username_usuario: partida.username_jugador!, fecha_conseguido: new Date()});
+    }
+  }
+
+  //Se verifica si es la primer partida perfecta del jugador (100%)
+  if (partida.puntaje! >= 1) {
+    //Se verifica que el jugador no tenga el logro
+    if (!verificarPosesionLogro(3, partida.username_jugador!)) {
+      //Si no lo tiene se le da
+      await LogroUsuario.query()
+      .insert({id_logro: 3, username_usuario: partida.username_jugador!, fecha_conseguido: new Date()});
+    }
+  }
+}
+
+//Verifica si un jugador tiene un logro
+//true: lo tiene
+//false: no lo tiene
+async function verificarPosesionLogro(id_logro: number, username_jugador: string) {
+    //Se verifica que el jugador no tenga el logro
+    const logro = await LogroUsuario.query()
+    .findById([2, username_jugador]);
+
+    if (logro === undefined) {
+      return false;
+    }
+
+    return true;
+}
