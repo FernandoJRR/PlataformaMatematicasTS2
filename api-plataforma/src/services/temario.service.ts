79bb1d8b44b2dcc689697aee8b1c060a01615c96
FernandoJRR <fercitotron@gmail.com>
2024-07-07 18:26:29 -0600

    add endpoints for patch temarios

--- api-plataforma/src/services/temario.service.ts
+++ api-plataforma/src/services/temario.service.ts
@@ -1,6 +1,7 @@
 import { Temario } from "../models/temario";
 import { Tema } from "../models/tema";
 import { Ejercicio } from "../models/ejercicio";
+import { timingSafeEqual } from "crypto";
 
 export async function getTemario(idTemario: number) {
   const temario = await Temario.query()
@@ -27,6 +28,13 @@ export async function getTemario(idTemario: number) {
 
 export async function getTemarios() {
   const temarios = await Temario.query();
+
+  for (const temario of temarios) {
+    const temas = await temario
+      .$relatedQuery('temas');
+    
+    temario.temas = temas;
+  }
 
   return temarios;
 }
@@ -108,8 +116,10 @@ export async function addTema(idTemario: number, inputTema: any) {
     }
     
     await trx.commit();
+    return tema;
   } catch (error) {
     await trx.rollback();
+    throw error;
   }
 }
 
@@ -148,4 +158,73 @@ export async function addEjercicio(idTema: number, inputEjercicio: any) {
     await trx.rollback();
   }
   
+}
+
+export async function updateTemario(idTemario: number, input: any) {
+  const temario = await Temario.query()
+    .findById(idTemario);
+
+  if (temario === undefined) {
+    throw new Error("El temario no existe");
+  }
+
+  const trx = await Temario.startTransaction();
+  try {
+    await Temario.query()
+    .where('id', idTemario)
+    .update(input);
+
+    await trx.commit();
+    return await Temario.query()
+      .findById(idTemario);
+  } catch (error) {
+    await trx.rollback();
+    throw error;
+  }
+}
+
+export async function updateTema(idTema: number, input: any) {
+  const tema = await Temario.query()
+    .findById(idTema);
+
+  if (tema === undefined) {
+    throw new Error("El tema no existe");
+  }
+
+  const trx = await Tema.startTransaction();
+  try {
+    await Tema.query()
+    .where('id', idTema)
+    .update(input);
+
+    await trx.commit();
+    return await Tema.query()
+      .findById(idTema);
+  } catch (error) {
+    await trx.rollback();
+    throw error;
+  }
+}
+
+export async function updateEjercicio(idEjercicio: number, input: any) {
+  const ejercicio = await Ejercicio.query()
+    .findById(idEjercicio);
+
+  if (ejercicio === undefined) {
+    throw new Error("El ejercicio no existe");
+  }
+
+  const trx = await Ejercicio.startTransaction();
+  try {
+    await Ejercicio.query()
+    .where('id', idEjercicio)
+    .update(input);
+
+    await trx.commit();
+    return await Ejercicio.query()
+      .findById(idEjercicio);
+  } catch (error) {
+    await trx.rollback();
+    throw error;
+  }
 }
