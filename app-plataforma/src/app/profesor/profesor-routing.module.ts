51c2f60aee519d091649ad61ebab77181ca6af90
FernandoJRR <fercitotron@gmail.com>
2024-07-08 19:35:12 -0600

    add partida jugada and promedio puntaje

--- app-plataforma/src/app/profesor/profesor-routing.module.ts
+++ app-plataforma/src/app/profesor/profesor-routing.module.ts
@@ -10,6 +10,7 @@ import { TemarioComponent } from './pages/temario/temario.component';
 import { EditarTemarioComponent } from './pages/editar-temario/editar-temario.component';
 import { EditarTemaComponent } from './pages/editar-tema/editar-tema.component';
 import { EditarEjercicioComponent } from './pages/editar-ejercicio/editar-ejercicio.component';
+import { EstadisticasProfesorComponent } from './pages/estadisticas-profesor/estadisticas-profesor.component';
 
 
 
@@ -39,6 +40,10 @@ const routes: Routes = [
       {
         path: 'editar-ejercicio', 
         component: EditarEjercicioComponent
+      },
+      {
+        path: 'estadisticas', 
+        component: EstadisticasProfesorComponent
       },
       {
         path: '**', 
---
+++ app-plataforma/src/app/profesor/services/estadisticas.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstadisticasService {
+  constructor(private http: HttpClient) {}
+
+  private baseUrl: string = 'http://localhost:3000';
+
+  /* PETICIONES A BACKEND */
+  // Obtener reporte partida de un profesor
+  obtenerReportePartidaProfesor(username_profesor: string): Observable<any> {
+    return this.http.get<any>(
+      `${this.baseUrl}/reporte/reportePartidaProfesor/${username_profesor}`
+    );
+  }
+}
